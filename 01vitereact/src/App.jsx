1a48b6aaee49cbfb295d04db0db3921749fcb57f
Grace Rosario <gracerosario398@gmail.com>
2026-06-07 18:50:20 +0530

    feat: implement componenets import and export

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <>
+      <h1>Chai aur react with vite | Farha Shaikh</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai() {
+    return(
+        <h1>I love chai with rose petals</h1>
+    )
+}
+
+export default Chai
